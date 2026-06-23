3f0edc1729484ed82f08b830503c734b99d916d3
Danilo Ribeiro Abranches dos Santos <danr.dossantos.work@gmail.com>
2026-06-22 23:44:38 -0300

    MongoDB databases

--- index.js
+++ index.js
@@ -1,32 +1,13 @@
+require('dotenv').config()
 const express = require('express')
-const app = express()
 const morgan = require('morgan')
 const cors = require('cors')
+const mongoose = require('mongoose')
+const Person = require('./models/person')
 
-let persons = [
-    {
-        "id": 1,
-        "name": "Arto Hellas",
-        "number": "040-123456"
-    },
-    {
-        "id": 2,
-        "name": "Ada Lovelace",
-        "number": "39-44-5323523"
-    },
-    {
-        "id": 3,
-        "name": "Dan Abramov",
-        "number": "12-43-234345"
-    },
-    {
-        "id": 4,
-        "name": "Mary Poppendieck",
-        "number": "39-23-6423122"
-    }
-]
+const app = express()
 
-app.use(express.json())
+let persons = []
 
 morgan.token('body', function (req) {
     if (req.body) {
@@ -36,34 +17,29 @@ morgan.token('body', function (req) {
 })
 
 app.use(morgan(':method :url :status :res[content-length] :response-time ms :body'))
-
 app.use(cors())
-
 app.use(express.static('dist'))
+app.use(express.json())
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1>')
 })
 
-app.get('/api/persons/', (request, response) => {
-    response.json(persons)
+app.get('/api/persons', (request, response) => {
+    Person.find({}).then((persons) => {
+        response.json(persons)
+    })
 })
 
 app.get('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    const person = persons.find(person => person.id === id)
-
-    if (person) {
-        response.json(person)
-    } else {
-        response.status(404).end()
-    }
+    Person.findById(request.params.id).then((person) => {
+        if (person) {
+            response.json(person)
+        } else {
+            response.status(404).end()
+        }
+    })
 })
-
-const generateId = () => {
-    const maxId = persons.length > 0 ? Math.max(...persons.map((n) => Number(n.id))) : 0
-    return Number(maxId + 1)
-}
 
 app.post('/api/persons', (request, response) => {
     const body = request.body
@@ -74,23 +50,14 @@ app.post('/api/persons', (request, response) => {
         })
     }
 
-    const name = persons.find(p => p.name === body.name)
-
-    if (name) {
-        return response.status(409).json({
-            error: 'name must be unique'
-        })
-    }
-
-    const person = {
-        id: generateId(),
+    const person = new Person({
         name: body.name,
         number: body.number
-    }
+    })
 
-    persons = persons.concat(person)
-
-    response.json(person)
+    person.save().then((savedPerson) => {
+        response.json(savedPerson)
+    })
 })
 
 app.get('/api/info', (request, response) => {
@@ -102,11 +69,18 @@ app.get('/api/info', (request, response) => {
 })
 
 app.delete('/api/persons/:id', (request, response) => {
-    const id = Number(request.params.id)
-    persons = persons.filter((person) => person.id !== id)
-    response.status(204).end()
+    Person.findByIdAndDelete(request.params.id).then((person) => {
+        response.status(204).end()
+    })
 })
 
-const PORT = process.env.PORT || 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
